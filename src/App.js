0e5579d85c55ec3ed0f9ce33f1069bd22dede2aa
Carlos A Ceolato Jr <carlos@ceolato.com.br>
2020-04-20 16:54:40 -0300

    Solução do desafio

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const repository = `Novo Repositório ${Date.now()}`;
+    const response = await api.post('repositories', {
+      title: repository,
+      url: 'https://github.com/cceolato/rocketseat-bootcamp-gostack-desafio03',
+      techs: ['NodeJS', 'ReactJS']
+    });
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    
+    const filteredRepos = repositories.filter(
+      repository => repository.id !== id
+    )
+
+    setRepositories(filteredRepos);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => 
+          <li key={repository.id}>
+              {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        )}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <form>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </form>
     </div>
   );
 }
